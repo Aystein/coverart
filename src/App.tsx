58b37bc6cd6b6787ab95b04e5018eb6331a28bb8
Moritz Schöfl <moritz.schoefl@datavisyn.io>
2021-12-01 13:50:38 +0100

    jj

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Ref, useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { API, Application, DatasetType, IVector, PluginRegistry, PSEContextProvider, PSEPlugin, rootReducer, RootState, setDatasetEntriesAction } from 'projection-space-explorer';
+import { API, Application, DatasetType, IVector, PluginRegistry, PSEContextProvider, PSEPlugin, rootReducer, RootState, setDatasetEntriesAction, CSVLoader, Dataset, RootActions, createRootReducer } from 'projection-space-explorer';
 import * as THREE from 'three'
 import { GLHeatmap } from './GLHeatmap';
 import { List, ListItem } from '@mui/material';
@@ -10,30 +10,9 @@ import { SoundPrint } from './SoundPrint';
 
 export const DATASETCONFIG = [
   {
-    display: "chess",
-    path: "chess16k.csv",
-    type: DatasetType.Chess
-
-  },
-  {
-    display: "Cycle 0",
-    path: "/jku-vds-lab/reaction-cime/datasets/reaction_optimization/no_suggestions/experiments_0.csv",
-    type: DatasetType.Experiments_Only
-  },
-  {
-    display: "Cycle 1",
-    path: "/jku-vds-lab/reaction-cime/datasets/reaction_optimization/no_suggestions/experiments_1.csv",
-    type: DatasetType.Experiments_Only
-  },
-  {
-    display: "Cycle 2",
-    path: "/jku-vds-lab/reaction-cime/datasets/reaction_optimization/no_suggestions/experiments_2.csv",
-    type: DatasetType.Experiments_Only
-  },
-  {
-    display: "Cycle 3",
-    path: "/jku-vds-lab/reaction-cime/datasets/reaction_optimization/no_suggestions/experiments_3.csv",
-    type: DatasetType.Experiments_Only
+    display: "Andreas Album",
+    path: "./coverart/names2.csv",
+    type: DatasetType.Sound
   }
 ]
 
@@ -62,37 +41,17 @@ PluginRegistry.getInstance().registerPlugin(new SoundPlugin())
 
 export function CIMEApp() {
   const api = //@ts-ignore
-    new API<RootState>(null, rootReducer)
+    new API<RootState>(null, createRootReducer({}))
 
   const [context] = useState(
     api
   );
 
 
-  api.store.dispatch(setDatasetEntriesAction(DATASETCONFIG))
 
   return (
     <PSEContextProvider context={context}>
       <Application
-        config={{
-          preselect: {
-            url: 'chess16k.csv'
-          }
-        }}
-        overrideComponents={{
-          layers: [{
-            order: -1,
-            component: () => <GLHeatmap
-              texture={new THREE.TextureLoader().load('test.jpg')}
-              size={{
-                x: 10,
-                y: 10,
-                width: 100,
-                height: 100
-              }}
-            ></GLHeatmap>
-          }]
-        }}
       />
     </PSEContextProvider>
   );
